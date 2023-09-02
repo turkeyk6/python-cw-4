337e4725e7ac090d8a8d94f39185234cf692ece2
turkeyk6 <142680770+turkeyk6@users.noreply.github.com>
2023-09-02 19:22:09 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+def my_name(name) :
+    print(f"my name is {name}")
+
+my_name ("turkey")
+
+def my_meal(food,drink):
+    print(f"i like to eat {food} while drinking {drink}")
+my_meal("pasta","water")
+
+def cube (number) :
+    return number*number*number
+value=cube(7)
+print(value)
+
+def by_three (number):
+    if value%3==0:
+        print(value)
+    else:
+        return False
+answer=by_three(6)
+print(answer)
